5bc5172d41aabcd879826871c105f2b3c3e22a2c
Raghurajadurai Tharmaraj <rtharmar@cisco.com>
2024-12-17 12:12:15 +0530

    Generic linux

--- thoorigai/install.sh
+++ thoorigai/install.sh
@@ -3,3 +3,8 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-macos-arm64.tar.gz
 tar xzf nvim-macos-arm64.tar.gz
 ./nvim-macos-arm64/bin/nvim
+
+# Linux
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage
