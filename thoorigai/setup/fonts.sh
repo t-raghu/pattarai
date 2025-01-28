bb8f6a545f48f4e9cd0b553083c13b0f8eb4134c
Raghu T <t.raghu@outlook.in>
2025-01-28 21:32:41 +0530

    Fonts

---
+++ thoorigai/setup/fonts.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf\n\n
